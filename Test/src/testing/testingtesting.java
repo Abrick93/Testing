b3305d4899fd25afddab979b1c40327759fbd558
amand <amand@192.168.1.127>
2022-01-22 17:36:39 -0700

    first commit

---
+++ Test/src/testing/testingtesting.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class testingtesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing testing");
+	}
+
+}
